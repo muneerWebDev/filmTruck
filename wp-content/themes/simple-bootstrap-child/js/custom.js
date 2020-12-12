78fae0f964dea4cd22abb138fdc798acfc638947
Muhammed Muneer <muneer@2hatslogic.com>
2020-12-12 10:53:48 +0530

    initial commit

---
+++ wp-content/themes/simple-bootstrap-child/js/custom.js
@@ -0,0 +1,24 @@
+
+jQuery(document).ready(function(){
+
+    containerMl = jQuery('.container').css("margin-left");
+    bannerVideoHeight =  jQuery(window).height() - jQuery('.banner').offset().top;
+    // alert(bannerVideoHeight);
+    jQuery('.banner-video .elementor-wrapper').css( 'padding-bottom' , bannerVideoHeight);
+    jQuery('.banner-video video').height(bannerVideoHeight);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ wp-content/themes/simple-bootstrap-child/photography.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Photography */ ?>
+
+<?php get_header(); ?>
+
+    <main class="photography">
+        <div class="row">
+                <div class="col-12">
+                    <div id="photos">
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php get_field('add_image'); ?>" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/1.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/2.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/3.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/7.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/4.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/8.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/5.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/6.jpg" alt="" class="img-fluid"> </a>
+                    <a href="Javascript:void(0);" class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/9.jpg" alt="" class="img-fluid"> </a>
+                    </div>
+                </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
